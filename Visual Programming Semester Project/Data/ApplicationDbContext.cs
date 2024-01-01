e9635bc92a112b01636173b0e0c714056f3003e0
Elee Lawleit <alibutt200235@gmail.com>
2024-01-02 00:49:53 +0500

    Initial Project Setup

---
+++ Visual Programming Semester Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Visual_Programming_Semester_Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Models.User> User { get; set; } = default!;
+    }
+}
---
+++ Visual Programming Semester Project/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Visual_Programming_Semester_Project.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string ImageUrl { get; set; }
+        public User() { }
+    }
+}
